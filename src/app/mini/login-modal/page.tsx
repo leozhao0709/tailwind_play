c515ae886a66546e59740e5b1cae1b25038237ab
leozhao0709 <leo.zhao.real@gmail.com>
2024-02-25 19:18:35 -0800

    minor update

--- src/app/mini/login-modal/page.tsx
+++ src/app/mini/login-modal/page.tsx
@@ -64,5 +64,5 @@ export const dynamic = 'auto';
 export const dynamicParams = true;
 export const revalidate = 60;
 export const fetchCache = 'auto';
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 export const preferredRegion = 'auto';
